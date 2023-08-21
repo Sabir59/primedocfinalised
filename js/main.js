3993375bf1012fae0215492522e6e479bc4e98ea
William James <wjames@icrowdnewswire.com>
2023-08-21 21:36:51 +0500

    commited

--- js/main.js
+++ js/main.js
@@ -51,3 +51,79 @@ $(document).ready(function () {
     $(".search-form").toggleClass("expanded");
   });
 });
+
+$(document).ready(function () {
+  let isWideScreen = $(window).width() > 991;
+
+  function updateSidebarPosition() {
+    // Get references to the elements
+    const $sidebar = $(".sidebar-content");
+    const $blogContent = $(".blog-card");
+
+    // Calculate the position where the sidebar should start scrolling
+    const startPosition = $blogContent.offset().top;
+    const bottomPosition = $blogContent.offset().bottom;
+
+    // Listen for the 'scroll' event on the window
+    $(window).scroll(function () {
+      const scrollY = $(window).scrollTop();
+
+      // Calculate the distance between the bottom of the blog content and the bottom of the viewport
+      const contentBottomPosition =
+        $blogContent.offset().top + $blogContent.outerHeight();
+      const viewportBottomPosition = scrollY + $(window).height();
+      const distanceToViewport =
+        contentBottomPosition - viewportBottomPosition;
+
+      if (scrollY <= startPosition && isWideScreen) {
+        $sidebar.css({
+          position: "fixed",
+          top: `${startPosition}px`,
+        });
+      } else if (distanceToViewport <= 0 && isWideScreen) {
+        $sidebar.css({
+          position: "relative",
+          top: "initial",
+          transition: "all .6s ease",
+        });
+      }
+    });
+  }
+
+  // Initial setup
+  updateSidebarPosition();
+
+  // Handle resizing
+  $(window).resize(function () {
+    const newScreenWidth = $(window).width();
+    if (isWideScreen && newScreenWidth <= 991) {
+      // Screen has become narrow, reset sidebar position
+      isWideScreen = false;
+      $(".sidebar-content").css({
+        position: "static",
+        top: "initial",
+        transition: "none",
+      });
+    } else if (!isWideScreen && newScreenWidth > 991) {
+      // Screen has become wide, update sidebar position
+      isWideScreen = true;
+      updateSidebarPosition();
+    }
+  });
+});
+
+// Utils
+// function scroll2FixedUntil(element2Target, element2Fix, viewPort) {}
+
+function textTrimmer(maxLength, pattern, elements) {
+  elements.each(function () {
+    const textContent = $(this).text();
+    const trimmedText =
+      textContent.length > maxLength
+        ? textContent.substring(0, maxLength) + pattern
+        : textContent;
+    $(this).text(trimmedText);
+  });
+}
+
+textTrimmer(300, "...", $(".blog-card__text"));
